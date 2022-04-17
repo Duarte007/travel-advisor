26c13acaaf167d543a1c0134af2b2692876329e6
Matheus Duarte <matheusdcarvalho007@gmail.com>
2022-04-17 17:26:13 -0300

    update map component to avoid renders

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -18,7 +18,6 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
             <GoogleMapReact
                 bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
                 defaultCenter={coordinates}
-                center={coordinates}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
                 options={{ disableDefaultUI: true, zoomControl: true }}
